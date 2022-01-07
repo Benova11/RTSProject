acd7313911b4596785a3f836142b415a8b8f5ddc
NISIM <NISIM550@GMAIL.COM>
2022-01-07 12:00:29 +0200

    3 handling more then one uint

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
+using System.Collections;
 
 public class Unit : NetworkBehaviour
 {
@@ -8,11 +10,50 @@ public class Unit : NetworkBehaviour
   [SerializeField] UnityEvent onSelected = null;
   [SerializeField] UnityEvent onDeselected = null;
 
-  public UnitMovements UnitMovements { get { return unitMovements; } }
 
-  #region Client
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
 
-  [Client]
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    public UnitMovements UnitMovements { get { return unitMovements; } }
+
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
+
+    #region Client
+
+    [Client]
   public void Select()
   {
     if (hasAuthority)
@@ -30,6 +71,25 @@ public class Unit : NetworkBehaviour
     }
   }
 
-  #endregion
+    #endregion
+    [Client]
+    public void select()
+    {
+        if (!hasAuthority) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void deselect()
+    {
+        if (!hasAuthority) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
 
 }
+
+
